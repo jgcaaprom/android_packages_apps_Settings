6c8c7c1d11a960666fb05250127b5a9ab1d5e9d2
George G <kreach3r@users.noreply.github.com>
2015-10-10 18:23:25 +0300

    Enable MultiWindow for everyone! :)
    Not just for userdebug & eng,  but user builds, too.

--- src/com/android/settings/DevelopmentSettings.java
+++ src/com/android/settings/DevelopmentSettings.java
@@ -1084,7 +1084,7 @@ public class DevelopmentSettings extends SettingsPreferenceFragment
     }
 
     private static boolean showEnableMultiWindowPreference() {
-        return !"user".equals(Build.TYPE);
+        return true;
     }
 
     private void setEnableMultiWindow(boolean value) {
